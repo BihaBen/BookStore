60d6ecfc3506ce342ecd0033417a7272dc9ccbb2
BihaBen <13.bence.b@gmail.com>
2026-02-13 22:07:25 +0100

    register site

---
+++ views/register.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="/BOOKSTORE/public/css/style.css">
+</head>
+
+<body>
+    <div class="panel">
+        <h2>Regisztráció</h2>
+
+        <?php if (!empty($error)): ?>
+            <p style="color:red; text-align:left;">
+                <?= htmlspecialchars($error) ?>
+            </p>
+        <?php endif; ?>
+
+        <form method="POST" action="?action=register">
+            <label for="username">Felhasználónév: (min. 3, max. 50 karakter)</label>
+            <input type="text" name="username" placeholder="Példa: TesztJakab" required>
+
+            <label for="email">Email cím: (létező email cím)</label>
+            <input type="email" name="email" placeholder="Példa: tesztElek@kitalalt.hu" required>
+
+            <label for="password">Jelszó: (min. 8 karakter: kis és nagybetű + szám)</label>
+            <input type="password" name="password" placeholder="Példa: PeldaJelszo1" required>
+            <button type="submit">Regisztráció</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
